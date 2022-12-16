c3b53c307c68c041d130d7fb86133676e7660b48
Yekong <yekongyt@gmail.com>
2022-12-16 14:39:16 +0800

    Change ping command to ping website

--- src/commands/utility.rs
+++ src/commands/utility.rs
@@ -1,16 +1,38 @@
 use serenity::framework::standard::{Args, CommandResult};
 use serenity::framework::standard::macros::command;
+use serenity::model::channel::Message;
+use serenity::model::Timestamp;
 use serenity::model::prelude::*;
 use serenity::prelude::*;
+use reqwest;
 
 #[command]
-#[description = "Pong!"]
-#[usage = ""]
-#[example = ""]
-#[aliases("pong")]
-#[bucket = "general"]
-async fn ping(ctx: &Context, msg: &Message, _args: Args) -> CommandResult {
-    msg.reply(ctx, "Pong!").await?;
-
+#[description = "Get the status of the website."]
+#[usage = "<url>"]
+#[example = "https://www.google.com"]
+#[min_args(1)]
+#[max_args(1)]
+#[bucket = "complicated"]
+#[aliases("status")]
+async fn ping(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
+    let url = args.single::<String>()?;
+    let client = reqwest::Client::new();
+    let res = client.get(&url).send().await.unwrap();
+    let status = res.status();
+    let msg_embed = msg
+        .channel_id
+        .send_message(&ctx.http, |m| {
+            m.embed(|e| {
+                e.title("Status");
+                e.description(format!("Status: {}", status));
+                e.timestamp(Timestamp::now())
+            });
+            m
+        })
+        .await;
+    // msg.channel_id.say(&ctx.http, format!("Status: {}", status)).await?;
+    if let Err(why) = msg_embed {
+        println!("Error sending message: {:?}", why);
+    }
     Ok(())
 }
